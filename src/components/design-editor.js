23309410fc6f29e331d9d0a1f404f6dff183ca5b
Christopher Brown <chbphone55@gmail.com>
2019-11-17 15:33:16 -0600

    getting there

--- src/components/design-editor.js
+++ src/components/design-editor.js
@@ -1,10 +1,38 @@
-import { html } from '/web_modules/lit-html.js';
-import { component } from '/web_modules/haunted.js';
+import { html, svg } from '/web_modules/lit-html.js';
+import { virtual, component, useMemo } from '/web_modules/haunted.js';
 
-function DesignEditor() {
-  return html``;
+const Rectangle = virtual(props => {});
+const Circle = virtual(props => {});
+const Text = virtual(props => {});
+
+const elementMapping = {
+  rectangle: Rectangle,
+  circle: Circle,
+  text: Text
+};
+
+function DesignEditor({ elements }) {
+  const svgElements = useMemo(
+    () =>
+      elements.map(element => {
+        return elementMapping[element.type]({
+          hidden: element.hidden,
+          styles: element.styles
+        });
+      }),
+    [elements]
+  );
+
+  return html`
+    <main>
+      ${svg`
+        <svg xmlns="http://www.w3.org/2000/svg">
+          ${svgElements}
+        </svg>
+      `}
+    </main>
+  `;
 }
 
 DesignEditor.observedAttributes = ['elements'];
-
 customElements.define('design-editor', component(DesignEditor));
