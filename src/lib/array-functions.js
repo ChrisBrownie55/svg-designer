d32f269a7cfb56cd98811bc04fa83fca2ab3fd16
Christopher Brown <chbphone55@gmail.com>
2019-11-17 01:36:04 -0600

    abstractions

---
+++ src/lib/array-functions.js
@@ -0,0 +1,13 @@
+export function removeByIndex(array, index) {
+  return array.filter((item, currentIndex) => currentIndex !== index);
+}
+
+export function replaceAtIndex(array, index, value) {
+  array.filter((item, currentIndex) => {
+    if (currentIndex === index) {
+      return value;
+    }
+
+    return item;
+  });
+}
