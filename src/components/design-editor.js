e168e42c8bc57ed1879d7e535c7e286a1b4f7853
Christopher Brown <chbphone55@gmail.com>
2019-11-17 01:35:58 -0600

    editors

--- src/components/design-editor.js
+++ src/components/design-editor.js
@@ -1,8 +1,8 @@
 import { html } from '/web_modules/lit-html.js';
 import { component } from '/web_modules/haunted.js';
 
-function StyleEditor() {
+function DesignEditor() {
   return html``;
 }
 
-customElements.define('style-editor', component(StyleEditor));
+customElements.define('design-editor', component(DesignEditor));
