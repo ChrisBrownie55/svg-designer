b26e27d6b9f54c9a8e4d34c26cbd493a058c3676
Christopher Brown <chbphone55@gmail.com>
2019-11-17 01:36:07 -0600

    yee

--- src/components/element-list.js
+++ src/components/element-list.js
@@ -1,15 +1,34 @@
 import { html } from '/web_modules/lit-html.js';
-import { component } from '/web_modules/haunted.js';
+import { component, useMemo } from '/web_modules/haunted.js';
 
 function ElementList({ elements, activeElement }) {
-  const elementList = elements.map(({ type }, index) => {
-    return html`
-      <button></button>
-    `;
-  });
+  const elementList = useMemo(
+    () =>
+      elements.map(({ type, hidden }, index) => {
+        const toggleHidden = () =>
+          this.dispatchEvent(
+            new CustomEvent('toggle-hidden', { detail: { index } })
+          );
+
+        const deleteElement = () =>
+          this.dispatchEvent(
+            new CustomEvent('delete-element', { detail: { index } })
+          );
+
+        return html`
+          <article class="ElementList__item" ?active=${index === activeElement}>
+            <h3>${type}</h3>
+            <button @click=${toggleHidden}>${hidden ? 'Show' : 'Hide'}</button>
+            <button @click=${deleteElement}>Delete</button>
+          </article>
+        `;
+      }),
+    [elements]
+  );
 
   return html`
-    <section>
+    <section class="ElementList">
+      <h2>Elements</h2>
       ${elementList}
     </section>
   `;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
 import { html } from '/web_modules/lit-html.js';
-import { component, useState } from '/web_modules/haunted.js';
+import { component, useState, useCallback } from '/web_modules/haunted.js';
+
+import './components/element-list.js';
+import { replaceAtIndex, removeByIndex } from './lib/array-functions.js';
 
 function App() {
+  const [elements, setElements] = useState([]);
+
+  const toggleHidden = useCallback(event => {
+    const { index } = event.detail;
+
+    const element = { ...elements[index] };
+    element.hidden = !element.hidden;
+
+    setElements(replaceAtIndex(elements, index, element));
+  });
+
+  const deleteElement = useCallback(
+    event => {
+      const { index } = event.detail;
+      setElements(removeByIndex(elements, index));
+    },
+    [elements]
+  );
+
   return html`
-    <div>Test</div>
+    <element-list
+      .elements=${elements}
+      @toggle-hidden=${toggleHidden}
+      @delete-element=${deleteElement}
+    ></element-list>
   `;
 }
 
