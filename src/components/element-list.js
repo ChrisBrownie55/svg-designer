0be88cd641898991584dc0f65ecedacee367f1c9
Christopher Brown <chbphone55@gmail.com>
2019-11-16 22:51:58 -0600

    working on it

--- src/components/element-list.js
+++ src/components/element-list.js
@@ -1,14 +1,20 @@
 import { html } from '/web_modules/lit-html.js';
 import { component } from '/web_modules/haunted.js';
 
-function ElementList({ elements, activeElement, setActiveElement }) {
-  return html``;
+function ElementList({ elements, activeElement }) {
+  const elementList = elements.map(({ type }, index) => {
+    return html`
+      <button></button>
+    `;
+  });
+
+  return html`
+    <section>
+      ${elementList}
+    </section>
+  `;
 }
 
-ElementList.observedAttributes = [
-  'elements',
-  'activeElement',
-  'setActiveElement'
-];
+ElementList.observedAttributes = ['elements', 'activeElement'];
 
 customElements.define('element-list', component(ElementList));
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { html } from '/web_modules/lit-html.js';
-import { component } from '/web_modules/haunted.js';
+import { component, useState } from '/web_modules/haunted.js';
 
 function App() {
   return html`
