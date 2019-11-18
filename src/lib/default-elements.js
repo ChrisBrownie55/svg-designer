62f03d8b4a6c1f7026dd83601e3706bcd526bfce
Christopher Brown <chbphone55@gmail.com>
2019-11-18 12:57:56 -0600

    include type names in default objects

--- src/lib/default-elements.js
+++ src/lib/default-elements.js
@@ -1,5 +1,6 @@
 export default Object.freeze({
   rectangle: Object.freeze({
+    type: 'rectangle',
     x: 0,
     y: 0,
     width: 100,
@@ -8,12 +9,14 @@ export default Object.freeze({
     rx: 0
   }),
   circle: Object.freeze({
+    type: 'circle',
     cx: 0,
     cy: 0,
     rx: 0,
     ry: 0
   }),
   text: Object.freeze({
+    type: 'text',
     x: 0,
     y: 0,
     lengthAdjust: 'spacing',
