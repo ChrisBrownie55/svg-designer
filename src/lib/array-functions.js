97740caf509f4832f6e3a3396fe330b20b799151
Christopher Brown <chbphone55@gmail.com>
2019-11-18 12:58:06 -0600

    return in replaceAtIndex

--- src/lib/array-functions.js
+++ src/lib/array-functions.js
@@ -3,7 +3,7 @@ export function removeByIndex(array, index) {
 }
 
 export function replaceAtIndex(array, index, value) {
-  array.filter((item, currentIndex) => {
+  return array.filter((item, currentIndex) => {
     if (currentIndex === index) {
       return value;
     }
