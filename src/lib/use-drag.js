bd8dea6832de1408e209ab687140c00cee3267da
Christopher Brown <chbphone55@gmail.com>
2019-11-19 13:17:39 -0600

    I think it's good

--- src/lib/use-drag.js
+++ src/lib/use-drag.js
@@ -5,24 +5,44 @@ export const DRAGGING = 'dragging';
 
 export default function useDrag() {
   const [state, setState] = useState(NOT_DRAGGING);
+  const [dragStart, setDragStart] = useState({ x: 0, y: 0 });
   const [positionDelta, setPositionDelta] = useState({ x: 0, y: 0 });
 
+  // on mousedown start dragging and set initial x and y positions
   function handleMouseDown(event) {
     setState(DRAGGING);
+    setDragStart({ x: event.clientX, y: event.clientY });
   }
 
   useEffect(() => {
     if (state === DRAGGING) {
-      function handleMouseMove(event) {}
+      // on mousemove recalculate the delta from the dragStart position
+      function handleMouseMove(event) {
+        setPositionDelta({
+          x: event.clientX - dragStart.x,
+          y: event.clientY - dragStart.y
+        });
+      }
       window.addEventListener('mousemove', handleMouseMove);
 
-      function handleMouseUp(event) {}
+      // on mouseup stop all dragging and reset the delta in position
+      function handleMouseUp() {
+        setState(NOT_DRAGGING);
+        setPositionDelta({ x: 0, y: 0 });
+      }
       window.addEventListener('mouseup', handleMouseUp);
 
+      // cleanup
       return () => {
         window.removeEventListener('mousemove', handleMouseMove);
         window.removeEventListener('mouseup', handleMouseUp);
       };
     }
+
+    return {
+      state,
+      dragStart,
+      positionDelta
+    };
   }, [state]);
 }
